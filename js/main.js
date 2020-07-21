25f16e9190e22253edd2f6676b162960fe198644
jaco9419 <jaco.9419@gmail.com>
2020-07-21 12:08:34 -0300

    Add typing effect, enhance images

--- js/main.js
+++ js/main.js
@@ -10,3 +10,44 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+const message = document.querySelector(".section__subtitle--intro");
+const words = ["software_developer    ", "let's_get_in_touch!    "];
+let i = 0;
+let timer;
+
+function typingEffect() {
+  let word = words[i].split("");
+  var loopTyping = function () {
+    if (word.length > 0) {
+      message.innerHTML += word.shift();
+    } else {
+      deletingEffect();
+      return false;
+    }
+    timer = setTimeout(loopTyping, 300);
+  };
+  loopTyping();
+}
+
+function deletingEffect() {
+  let word = words[i].split("");
+  var loopDeleting = function () {
+    if (word.length > 0) {
+      word.pop();
+      message.innerHTML = word.join("");
+    } else {
+      if (words.length > i + 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      typingEffect();
+      return false;
+    }
+    timer = setTimeout(loopDeleting, 200);
+  };
+  loopDeleting();
+}
+
+typingEffect();
