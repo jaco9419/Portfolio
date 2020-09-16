20d5228d719f6f86c208a2e6bda04370cf49f866
jaco9419 <jaco.9419@gmail.com>
2020-09-15 23:45:34 -0300

    Add data.js for better portfolio items rendering

---
+++ js/data.js
@@ -0,0 +1,27 @@
+export const portfolioItems = [
+    {
+        href: 'https://jaco9419.github.io/Netflix-Land-Page/',
+        title: 'Netflix Land Page',
+        technologies: 'HTML, CSS, Javascript',
+        src: 'img/8.PNG',
+    },
+    {
+        href: 'https://jaco9419.github.io/Netflix-Land-Page/',
+        title: 'Netflix Land Page',
+        technologies: 'HTML, CSS, Javascript',
+        src: 'img/8.PNG',
+    },
+    {
+        href: 'https://jaco9419.github.io/Netflix-Land-Page/',
+        title: 'Netflix Land Page',
+        technologies: 'HTML, CSS, Javascript',
+        src: 'img/8.PNG',
+    },
+    {
+        href: 'https://jaco9419.github.io/Netflix-Land-Page/',
+        title: 'Netflix Land Page',
+        technologies: 'HTML, CSS, Javascript',
+        src: 'img/8.PNG',
+    },
+]
+
--- js/main.js
+++ js/main.js
@@ -1,53 +1,93 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav__link");
+import { portfolioItems } from './data.js';
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
+//Handle Menu
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-  });
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    });
 });
 
-const message = document.querySelector(".section__subtitle--intro");
-const words = ["software_developer    ", "let's_get_in_touch!    "];
+//Handle data for portfolio items
+
+const portfolio = document.querySelector('.portfolio');
+const a = document.createElement('a');
+const p = document.createElement('p');
+
+portfolioItems.map(item => {
+  portfolio.innerHTML += `
+  <a
+          href=${item.href}
+          class="portfolio__item"
+          target="_blank"
+        >
+          <p class="portfolio__description">
+            Netflix Land Page<br /><br />HTML, CSS, Javascript
+          </p>
+          <img
+            src=${item.src}
+            alt="Netflix-Land-Page Project"
+            class="portfolio__img"
+          />
+        </a>
+  `
+})
+
+//a.innerHTML = `<p>${portfolioItems[0].href}</p>`
+
+// a.setAttribute('href', portfolioItems[0].href);
+// a.setAttribute('class', 'portfolio__item');
+// a.setAttribute('target', '_blank');
+// p.setAttribute('class', 'portfolio__description');
+// a.appendChild(p);
+//portfolio.appendChild(a);
+
+//Typing Effect
+
+const message = document.querySelector('.section__subtitle--intro');
+const words = ['software_developer    ', "let's_get_in_touch!    "];
 let i = 0;
 let timer;
 
 function typingEffect() {
-  let word = words[i].split("");
-  var loopTyping = function () {
-    if (word.length > 0) {
-      message.innerHTML += word.shift();
-    } else {
-      deletingEffect();
-      return false;
-    }
-    timer = setTimeout(loopTyping, 300);
-  };
-  loopTyping();
+    let word = words[i].split('');
+    var loopTyping = function () {
+        if (word.length > 0) {
+            message.innerHTML += word.shift();
+        } else {
+            deletingEffect();
+            return false;
+        }
+        timer = setTimeout(loopTyping, 300);
+    };
+    loopTyping();
 }
 
 function deletingEffect() {
-  let word = words[i].split("");
-  var loopDeleting = function () {
-    if (word.length > 0) {
-      word.pop();
-      message.innerHTML = word.join("");
-    } else {
-      if (words.length > i + 1) {
-        i++;
-      } else {
-        i = 0;
-      }
-      typingEffect();
-      return false;
-    }
-    timer = setTimeout(loopDeleting, 200);
-  };
-  loopDeleting();
+    let word = words[i].split('');
+    var loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            message.innerHTML = word.join('');
+        } else {
+            if (words.length > i + 1) {
+                i++;
+            } else {
+                i = 0;
+            }
+            typingEffect();
+            return false;
+        }
+        timer = setTimeout(loopDeleting, 200);
+    };
+    loopDeleting();
 }
 
 typingEffect();
